275efb48e19b6c4ed764731c8950551a1b31944a
corvettejames <corvettejames@icloud.com>
2023-03-19 19:01:17 -0400

    finished :)

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Code your solution!")
+const formElement = document.getElementById('Vette')
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let listElement = document.createElement('li');
+    const textInputValue = event.target.testInput.value;
+  
+    listElement.textContent = textInputValue;
+
+    listElement.addEventListener('click', handleClick =>{
+     handleClick.target.style.textDecoration = "line-through";
+    })
+
+    const unorderedListElement = document.querySelector('ul');
+    unorderedListElement.append(listElement);
+    event.target.testInput.value ='';
+
+})
