d83546e54e685ea90728f332f41be1781130153d
salvagebar <salvagebar@gmail.com>
2023-03-19 16:51:53 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Code your solution!")
+const list = document.querySelector("form");
+
+list.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const unOrdered = document.querySelector("ul");
+  const listText = document.getElementById("list").value;
+  if (listText) {
+    const newLi = document.createElement("li");
+  }
+});
