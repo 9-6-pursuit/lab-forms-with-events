2dcbdf188cb2dfd95ed7bf0cf74f8f4292b27733
Justin Knolley <justinknolley@pursuit.org>
2023-03-19 18:48:36 -0400

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Code your solution!")
+const list = document.querySelector("form")
+list.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+
+const unOrdered = document.querySelector("ul")
+const listText = document.getElementById("list").value;
+if (listText) {
+    const newLi = document.createElement("li")
+    newLi.textContent = listText
+    unOrdered.append(newLi)
+    newLi.addEventListener("click", (event) => {
+        newLi.style.textDecoration = "line-through"
+    })
+} else document.querySelector("p").textContent = "Error! To-Do Cannot Be Empty"
+})
