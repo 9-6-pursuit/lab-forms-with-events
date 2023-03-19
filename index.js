f6383687e44d7f93b7fb62b46fdcf43cbdb31354
salvagebar <salvagebar@gmail.com>
2023-03-19 17:29:20 -0400

    Styled page; form fields reset on submission

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const list = document.querySelector("form");
+let warningLine = document.getElementById("warning");
+let warned = false;
 
 list.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const unOrdered = document.querySelector("ul");
+    
+  const unOrdered = document.querySelector("ul"); // the unordered list where the todo items go
+  let toDoForm = document.getElementById("todo"); // var for form element by id
   let listText = document.getElementById("list").value;
   if (listText) {
-    const newLi = document.createElement("li");
-    newLi.textContent = listText;
-    unOrdered.append(newLi);
+    const newLine = document.createElement("li"); // the new list element with the new todo item
+    newLine.textContent = listText;
+    unOrdered.append(newLine);
+  } else {
+    warningLine.innerText = "Enter some text. This field cannot be left blank.";
+    warned = true;
   }
-  listText = "";
+  toDoForm.reset(); // resets the form on submission
 });
