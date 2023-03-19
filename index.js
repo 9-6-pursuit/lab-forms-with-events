02c9173a23ea71be3da6f68cf91afb5f9c2cde3d
salvagebar <salvagebar@gmail.com>
2023-03-19 17:08:09 -0400

    Basic functionality achieved

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ list.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const unOrdered = document.querySelector("ul");
-  const listText = document.getElementById("list").value;
+  let listText = document.getElementById("list").value;
   if (listText) {
     const newLi = document.createElement("li");
+    newLi.textContent = listText;
+    unOrdered.append(newLi);
   }
+  listText = "";
 });
