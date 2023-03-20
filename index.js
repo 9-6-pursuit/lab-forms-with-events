e70a20c4cbe6b8d243facc1a3b987f3d5a0c9d1d
salvagebar <salvagebar@gmail.com>
2023-03-20 14:33:59 -0400

    Tweaks

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ list.addEventListener("submit", (event) => {
   }
   const unOrdered = document.querySelector("ul"); // the unordered list where the todo items go
   let toDoForm = document.getElementById("todo"); // var for form element by id
-  let listText = document.getElementById("list").value;
+  let listText = document.getElementById("list").value; // what the user inputs
   if (listText) {
     const newLine = document.createElement("li"); // the new list element with the new todo item
     newLine.textContent = listText;
