ee79e3a75cd7f1c6fbf924a5ba2c2b84b1ef1e2e
crsvalle <cristian.valle@pursuit.org>
2023-03-19 20:10:54 -0400

    Base done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Code your solution!")
+const list = document.querySelector("form");
+const unordered = document.querySelector("ul");
+const listT = document.querySelector('input');
+const deleteButton = document.querySelectorAll("button")[1]
+
+list.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+if (listT){
+    const newList = document.createElement("li")
+    newList.textContent = listT.value
+    unordered.append(newList)
+
+    deleteButton.addEventListener("click", () => {
+        newList.remove()
+    })
+
+}
+
+unordered.addEventListener("click", (event) => {
+        event.target.style.textDecoration = 'line-through solid'
+})
+})
