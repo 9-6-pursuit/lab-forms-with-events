42518a01e5d1ea13c08aa40d94b4ed24af41fc55
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-03-20 17:44:47 -0400

    idk how to make it work

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Code your solution!")
+const list = document.querySelector("form");
+list.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+    const unOrdered = document.querySelector("ul")
+    const listText = document.getElementById("list").value
+    console.log(listText)
+    if(listText){
+        const newLi = document.createElement("li")
+        newLi.textContent = listText
+        unOrdered.append(newLi)
+    } else {
+        const err = document.querySelector("p")
+        err.textContent = "Error! to-do cannot be empty"
+        p.append(err)
+    }
+})
+
+
+unOrdered.addEventListener("click",(event2)=>{
+        event2.target.style.textDecoration = 'line-through';
+    }
+)
