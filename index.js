31e3c66e49eec711dac26727d35b7710b57bb577
Wai leong Chong <waileongchong@wais-air.mynetworksettings.com>
2023-03-19 23:42:09 -0400

    forms with events lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("Code your solution!")
+let list = document.querySelector("#list");
+let unOndered = document.querySelector("#task-list");
+let textInput = document.querySelector("#text");
+let error = document.querySelector("p");
+
+list.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  let task = textInput.value;
+  if (!task) {
+    error.textContent = "Error. Todo cannot be empty";
+    return;
+  }
+
+  let newLi = document.createElement("li");
+  newLi.textContent = task;
+  unOndered.append(newLi);
+
+  textInput.value = "";
+  error.textContent = "";
+});
+
+unOndered.addEventListener("click", function(event) {
+  let clickedListItem = event.target;
+  if (clickedListItem.tagName === "LI") {
+    if (clickedListItem.style.textDecoration === "line-through") {
+      clickedListItem.style.textDecoration = "none";
+    } else {
+      clickedListItem.style.textDecoration = "line-through";
+    }
+  }
+});
+
+
+
+
+
+
+
