c1b1af070cd1b52f6c7548ffbeb37c1656b34e51
Alejandra <alejandraramirez@pursuit.org>
2023-03-20 20:41:55 -0400

    Lab Forms with events Done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Code your solution!")
+console.log ()
+let form=document.getElementById("formu");
+let input=document.getElementById("inp");
+let list=document.getElementById("list");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+
+    let msg=document.getElementById("msg");
+    if (input.value=="") {
+        msg.textContent="Can't be empty";
+    } else {
+        msg.textContent="";
+        let elemento=document.createElement("li");
+        elemento.textContent=input.value;
+        input.value="";
+        elemento.addEventListener("click",()=>{
+            elemento.setAttribute("class","rayar")
+        })
+        list.appendChild(elemento);
+    }
+})
