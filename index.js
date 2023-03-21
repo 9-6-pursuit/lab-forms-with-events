957396d8d90d48c25a7357ad12a9f3ffccb0acd4
Lakisha J <lakishajohnson@pursuit.org>
2023-03-21 12:51:37 -0400

    Added delete button

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 //console.log("Code your solution!")
 
 //This function creates an li, adds a textNode to li, and returns li
-function contactTemplate(item) {
+function allListItems(item) {
     const li = document.createElement("li");
     li.textContent += item;
     return li;
 };
-//This function assigns above function to the li. It, selects the ul and appends li as last child of ul(PUTS LI INSIDE OF UL)
-function generateContact(item) {
-    const li = contactTemplate(item);
+//This function assigns above function to the li. Then selects the ul and appends li as last child of ul(PUTS LI INSIDE OF UL)
+function generateList(item) {
+    const li = allListItems(item);
     const ul = document.querySelector("ul");
     ul.append(li);
 };
 //Selects body element
 const body = document.querySelector('body');
-//Creates a form tag and set an attributes
-const form = document.createElement('form');
-form.setAttribute('id', 'form-content')
 //Creates h1 tag and adds a textNode to h1 tag, then appends h1 to the body(PUTS H1 INSIDE BODY) 
 const h1 = document.createElement('h1');
 h1.textContent = "My To-Dos";
 body.append(h1);
 
+//Creates a form tag and set attributes
+const form = document.createElement('form');
+form.setAttribute('id', 'form-content')
 //Creates an input tag and sets attributes for tag, then appends input tag to the form
 const input = document.createElement('input');
-//setting key/pair values as attributes to input tag (name)key, (userInput)value
+//setting key/pair values as attributes to input tag....... (name)key, (userInput)value
 input.setAttribute('name', 'userInput')
 input.setAttribute('id', 'name')
 input.setAttribute('type', 'text')
@@ -44,9 +44,9 @@ const paragraph = document.createElement('p');
 body.append(paragraph);
 
 //Creates an ul tag
-const unOrderedList = document.createElement('ul');
+const ul = document.createElement('ul');
 //Appends ul as a sibling of the form tag
-form.after(unOrderedList);
+form.after(ul);
 
 //Selects the form tag by its id
 const formQuery = document.querySelector("#form-content");
@@ -61,35 +61,47 @@ formQuery.addEventListener("submit", (event) => {
     paragraph.textContent = "Please enter an item"; 
     } else { 
         //Call above function that selects ul and appends li as a child of unordered list
-        generateContact(item);
+        generateList(item);
         //Resets the input field of above function
         event.target.reset()
     }
 });
     //Appends ul tag to form tag
-form.append(unOrderedList);
-    
+form.append(ul);
+
+//TOGGLE ON/OFF LINE-THRU TEXT DECORATION
+let isLineThrough = false;    
     //Selects unordered list to add an EventListener w/2 arguments
 document.querySelector("ul").addEventListener("click", (event) => {
-    //Expected to stop the default behavior of clicking on ul
-    event.preventDefault();
-    //When target ul is clicked a text-decoration will be applied to that ul
-    event.target.style["text-decoration"] = "line-through";
+        //Expected to stop the default behavior of clicking on ul
+        event.preventDefault();
+        //When target ul is clicked a text-decoration will be applied to that ul
+        if (isLineThrough) {
+        event.target.style["text-decoration"] = "";
+        isLineThrough = false;
+    } else {
+        event.target.style["text-decoration"] = "line-through";
+        isLineThrough = true;
+    }
 });
-// //make a button
-// const delBut = document.createElement("button");
-// //set name on button
-// delBut.textContent = "Delete";
-// //put button at the end of the li element
-// form.append(delBut);
-// //tell button what to do when clicked
-// delBut.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     //remove the whole list item
-//     li.remove();
-// })
 
-/*const input = document.querySelector('input')
+//DELETE BUTTON
+const delBut = document.createElement("button");
+//set name on button
+delBut.textContent = "Delete";
+//put button at the end of the li element
+form.append(delBut);
+//tell button what to do when clicked
+delBut.addEventListener("click", (event) => {
+    event.preventDefault();
+    let ul = document.querySelector("ul")
+    ul.innerHTML = ""
+    //remove the whole list item
+});
+
+//ALTERNATIVE SOLUTION USING HTML ALSO//
+/*
+const input = document.querySelector('input')
 const button = document.querySelector('button')
 const form = document.querySelector('form')
 
@@ -107,7 +119,7 @@ form.addEventListener("submit", (event) => {
         myList.style.textDecoration = "line-through solid";
     })
 })
-form.reset();
+    form.reset();
 })*/
 
 
