9be2c67021e9f31fb8890be5e35d39ec42ade5ad
Berlin Rivas <berlinrivas@Berlins-MacBook-Air.local>
2023-03-22 04:08:03 -0400

    We have achieve greatness

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Code your solution!")
+
+const form = document.querySelector('form')
+    form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+const unorderedList = document.querySelector('ul')
+const listText = document.getElementById('list').value
+
+// const paragraph = document.querySelector()
+
+if(listText) {
+    const newLi = document.createElement('li')
+    newLi.textContent = listText
+    unorderedList.append(newLi)
+    }
+
+if(listText === '') {
+    const newList = document.querySelector('p')
+    newList.textContent = 'Error your todo list is empty!'
+    unorderedList.append(newList)
+    }
+
+})
