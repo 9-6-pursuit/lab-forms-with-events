bfe954d17d81c5d2d2103257978907aa50658c66
Phillip Andrew Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-03-21 22:01:06 -0400

    visual test passing I think

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Code your solution!")
+const list = document.querySelector("form");
+list.addEventListener("submit", (event) => {
+event.preventDefault();
+
+const unOrdered = document.querySelector("ul")
+const listText = document.getElementById("list").value;
+if (listText){
+const newLi = document.createElement("li")
+newLi.textContent =listText
+unOrdered.append(newLi)
+newLi.addEventListener("click", (event) => {
+    event.preventDefault()
+    newLi.style.textDecoration ="line-through"
+})
+} 
+
+else{
+    const paragraph = document.querySelector("p")
+    paragraph.textContent = "Error: Todo cannot be empty"
+}
+})
