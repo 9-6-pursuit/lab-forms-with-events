245d0d0bcf6b41444510c9c33934a6d23123c957
Monir Hossen <monirhossen@pursuit.org>
2023-03-26 14:37:06 -0400

    formsWevents

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Code your solution!")
+
+
+const form = document.querySelector('form')
+    form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+const unorderedList = document.querySelector('ul')
+const listText = document.getElementById('list').value
+
+if(listText) {
+    const list = document.createElement('li')
+    list.textContent = listText
+    unorderedList.append(list)
+    }
+if(listText === '') {
+    const newList = document.querySelector('p')
+    newList.textContent = "Error. Todo cannot be empty"
+    unorderedList.append(newList)
+    }
+
+})
